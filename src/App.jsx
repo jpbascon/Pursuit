108d869f06adbb60c487a02b81bab0b11f30b64f
jpbascon <jpbascon50@gmail.com>
2025-08-27 16:59:58 +0800

    tweaked colors from white to dirty gray-white, changed my picture, and added a forgot password component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Footer from './components/Footer';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Contact from './components/Contact';
+import ForgotPassword from './components/ForgotPassword';
 import LandingPage from './components/LandingPage';
 import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -20,6 +20,7 @@ function App() {
             <Route path="/login" element={<Login />}></Route>
             <Route path="/signup" element={<Signup />}></Route>
             <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/forgot-password" element={<ForgotPassword />}></Route>
           </Routes>
         </main >
         <Footer />
