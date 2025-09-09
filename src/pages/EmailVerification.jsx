999581efabe341ca7cf6f149f5b61b19bc85ed25
jpbascon <jpbascon50@gmail.com>
2025-09-09 17:23:21 +0800

    Final fix

--- src/pages/EmailVerification.jsx
+++ src/pages/EmailVerification.jsx
@@ -9,14 +9,16 @@ const EmailVerification = () => {
     <>
       <div className="pt-15 max-w-7xl mx-auto">
         <div className="flex flex-col gap-5 items-center relative z-20">
-          <h1 className="text-3xl">{status === "Success" ? "Email Verified" : "Verification failed"}</h1>
+          <h1 className="text-3xl">{status === "success" ? "Email Verified" : "Verification failed"}</h1>
           <div>
-            <p>{status === "success" &&
-              `Email Verified. You can now 
-              ${<button className="underline"
-                onClick={() => navigate("/login")}>log in</button>} 
-                your account.</p>`}
-            </p>
+            {status === "success" &&
+              <p>Email Verified. You can now
+                <button className="underline"
+                  onClick={() => navigate("/login")}>
+                  log in</button>
+                your account.
+              </p>
+            }
           </div>
         </div>
       </div>
