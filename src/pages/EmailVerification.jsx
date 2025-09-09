ccdd174515bf9804e6faeb8ba04c487b03195a12
jpbascon <jpbascon50@gmail.com>
2025-09-09 17:12:51 +0800

    verifyUrl test 9

--- src/pages/EmailVerification.jsx
+++ src/pages/EmailVerification.jsx
@@ -1,19 +1,22 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const EmailVerification = () => {
-  const [searchParams] = useSearchParams();
-  const status = searchParams.get("status");
+  const { search } = useLocation();
+  const params = new URLSearchParams(search);
+  const status = params.get("status");
   const navigate = useNavigate();
   return (
     <>
       <div className="pt-15 max-w-7xl mx-auto">
         <div className="flex flex-col gap-5 items-center relative z-20">
-          <h1 className="text-3xl">Email Verified!</h1>
+          <h1 className="text-3xl">{status === "Success" ? "Email Verified" : "Verification failed"}</h1>
           <div>
-            <p>{status === "success" ? `Email Verified. You can now 
+            <p>{status === "success" &&
+              `Email Verified. You can now 
               ${<button className="underline"
-                onClick={() => navigate("/login")}>log in</button>} your account.</p>` :
-              `${"Verification failed"}`}</p>
+                onClick={() => navigate("/login")}>log in</button>} 
+                your account.</p>`}
+            </p>
           </div>
         </div>
       </div>
